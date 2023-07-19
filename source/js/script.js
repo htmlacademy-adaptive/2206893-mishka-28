02940cd5ce86e1bca135392d719c2687ee12327a
tanya.sille <tanya.sille@gmail.com>
2023-07-20 00:21:42 +0300

    заканчивает стилизацию с сетками страницы форм и добавляет интерактивность слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -20,5 +20,48 @@ const initMap = () => {
   mapFrame.style.display = "block";
 };
 
+const initSlider = () => {
+  const sliderButtonPrev = document.querySelector('.review__slider-button--prev');
+  const sliderButtonNext = document.querySelector('.review__slider-button--next');
+  const slides = document.querySelectorAll('.review__slider-item');
+
+  let currentSlide = 0;
+
+  if (!slides.length) {
+    return;
+  }
+
+  const removeSlideActiveState = () => {
+    slides[currentSlide].classList.remove('review__slider-item--active');
+  };
+
+  const addSlideActiveState = () => {
+    slides[currentSlide].classList.add('review__slider-item--active');
+  };
+
+  sliderButtonPrev.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    removeSlideActiveState();
+    if (currentSlide === 0) {
+      currentSlide = slides.length - 1;
+    } else {
+      currentSlide -= 1;
+    }
+    addSlideActiveState();
+  });
+
+  sliderButtonNext.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    removeSlideActiveState();
+    if (currentSlide === slides.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide += 1;
+    }
+    addSlideActiveState();
+  });
+};
+
 initOpenMenu();
 initMap();
+initSlider();
